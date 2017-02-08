9d29f2c353d7cb3f206753556481610e6fc1ed8c
Anton Matrosov <underlight@mail.ru>
2017-02-09 00:07:36 +0300

    Port 8080

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ server.post ('/findmyrep', findmyrep.handle);
 server.get ('/findmyrep', findmyrep.handle);
 
 
-server.listen (process.env.PORT || 5000, function ()
+server.listen (process.env.PORT || 8080, function ()
 {
     console.log ('>> Server listening');
     console.log ('-------------------------------');
